bc1261e4c0d72ec5c1a0f7ca80b7ba78a5c5b364
kauverysreerejendran <kauverysree@pinesphere.com>
2026-02-18 09:58:33 +0530

    DP - Draft: Lot Status & Auto Suggestion Off - Fix

--- DayPlanning/views.py
+++ DayPlanning/views.py
@@ -43,8 +43,6 @@ from django.utils.decorators import method_decorator
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer
-import math
-import json
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from .models import *
